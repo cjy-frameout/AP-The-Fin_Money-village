865642338bda9801a6770a126bf6e8694541c8a4
jugumtong0105 <jugumtong@naver.com>
2023-08-18 13:48:49 +0900

    핀스토어 페이지 작업

--- js/interaction.js
+++ js/interaction.js
@@ -15,10 +15,34 @@
                 });
             });
         });
-    }
+    },
 
+    $.fn.passChange = function(){
+        return this.each(function(){
+            var $this = $(this);
+            var count = 0;
+            var disc = $this.parents().find('.password-disc-wrap');
+            console.log(disc);
+            $this.keydown(function(e){
+                if(e.key === "Backspace" || e.key == "Delete") {
+                    count--;
+                    disc.find('span').last().remove();
+                }
+                else if(e.key == 0 || e.key == 1 || e.key == 2 || e.key == 3 || e.key == 4 || e.key == 5 || e.key == 6 || e.key == 7 || e.key == 8 || e.key == 9 ) {
+                    
+                    count++;
+                    disc.append("<span class='pass-disc'></span>"); 
+                } else {} 
+                console.log(count);
+                
+            });
+        });
+    }
+    
 })(jQuery);
 
 document.addEventListener('DOMContentLoaded', function(e){
     $('.btn-open-layerPop').layerPopOpen();
+    $('input[type="password"]').passChange();
 });
+
