b771bda5f0c3c065dd572ecaed122c8dc60a7819
jugumtong0105 <jugumtong@naver.com>
2023-10-25 11:19:03 +0900

    패스워드 수정

--- js/interaction.js
+++ js/interaction.js
@@ -30,6 +30,8 @@
                         count++;
                         disc.append("<span class='pass-disc'></span>");
                     }
+                    if (count <= 0) $this.css("opacity", 1);
+                    else $this.css("opacity", 0);
                 });
             });
         }),
