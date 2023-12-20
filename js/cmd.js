b1fb51f01fb40281dc33a0ec1626be6c1c49c039
jugumtong0105 <jugumtong@naver.com>
2023-12-20 09:14:59 +0900

    스크립트 수정 및 css 수정

--- js/cmd.js
+++ js/cmd.js
@@ -47,78 +47,14 @@ function btnFolding() {
 }
 
 
-function dragInner(){
-    if ($(".sortCtrl").length) {
-        $(".sortCtrl")
-            .children()
-            .first()
-            .addClass("on")
-            .find("a")
-            .attr("title", "선택됨");
-        $(".sortChg").each(function () {
-            $(this).children().first().addClass("on");
-        });
-        if ($(".editList").length) {
-            dragSortList();
-        }
-        $(".sortCtrl")
-            .find("a")
-            .click(function () {
-                var idx = $(this).parent().index();
-                $(this)
-                    .attr("title", "선택됨")
-                    .parent()
-                    .addClass("on")
-                    .siblings()
-                    .removeClass("on")
-                    .find("a")
-                    .removeAttr("title");
-                $(".sortChg").each(function () {
-                    $(this)
-                        .children()
-                        .eq(idx)
-                        .addClass("on")
-                        .siblings()
-                        .removeClass("on");
-                });
-                if ($(".editList").length) {
-                    if (idx === 0) {
-                        $(".asList").children().removeClass("selected");
-                        dragSortList();
-                    } else {
-                        $(".asList").sortable("destroy");
-                        $(".asList").children().removeClass("selected");
-                    }
-                    $(".asList")
-                        .children()
-                        .click(function () {
-                            if (idx === 1) {
-                                $(".asList").children().removeClass("selected");
-                                $(this).addClass("selected");
-                            } else {
-                                $(this).removeClass("selected");
-                            }
-                        });
-                }
-                if ($(".payList").length) {
-                    if (idx === 1) {
-                        $(".payList").addClass("idList");
-                    } else {
-                        $(".payList").removeClass("idList");
-                    }
-                }
-                return false;
-            });
-    }
-}
+
 function dragSort() {
-    function dragSortList() {
-        $(".asList").sortable({
-            axis: "y",
-            handle: ".btnHandle",
-            scroll: false,
-        });
-    }
+    $(".asList").sortable({
+        axis: "y",
+        handle: ".btnHandle",
+        scroll: false,
+    });
+   
     
     $(".accItem").each(function () {
         if ($(this).hasClass("blockLink")) {
@@ -131,9 +67,82 @@ function dragSort() {
     });
 }
 
+
+function setDatepicker(target) {
+    var widgetHeader = target
+        .datepicker("widget")
+        .find(".ui-datepicker-header");
+    var prevYrBtn = $(
+        '<a class="ui-datepicker-prevY" data-handler="prevY"><span class="ui-icon">Prev Year</span></a>'
+    );
+    var nextYrBtn = $(
+        '<a class="ui-datepicker-nextY" data-handler="nextY"><span class="ui-icon">Next Year</span></a>'
+    );
+    nextYrBtn.appendTo(widgetHeader);
+    prevYrBtn.prependTo(widgetHeader);
+    prevYrBtn.unbind("click").bind("click", function () {
+        $.datepicker._adjustDate(target, -1, "Y");
+    });
+    nextYrBtn.unbind("click").bind("click", function () {
+        $.datepicker._adjustDate(target, +1, "Y");
+    });
+    if (target.hasClass("bg_dim")) {
+        $("body").append('<div class="dim"></div>');
+    }
+}
+
+// S :   calendar
+var $focusReturn2;
+function beforeShow(input, e){
+    var self = $(input);
+    var title = self.attr("data-title");
+    setTimeout(function () {
+        self.prop("readonly", true);
+        setDatepicker(self);
+        if (typeof title !== "undefined") {
+            $(".ui-datepicker-header").prepend(
+                '<p class="ui_datepicker_tit" tabindex="0">' +
+                    title +
+                    "</p>"
+            );
+        }
+        $("#ui-datepicker-div")
+            .attr({ tabindex: "0", "aria-hidden": "false" })
+            .focus();
+        $focusReturn2 = self.siblings(".ui-datepicker-trigger");
+        $(".ui-datepicker-year").attr("title", "년도선택");
+        $(".ui-datepicker-month").attr("title", "월선택");
+
+        if (!$(".dim").length) {
+            $(".wrap").append('<div class="dim"></div>');
+        }
+    }, 1);
+
+    // 220502 레이어 높이가 달력보다 높은 case 때문에 생성
+    $(".layer_inner").css({ opacity: "0" });
+}
+
+function onChangeMonthYear(){
+    var self = $(this);
+    var title = self.attr("data-title");
+    setTimeout(function () {
+        setDatepicker(self);
+        if (typeof title !== "undefined") {
+            $(".ui-datepicker-header").append(
+                '<p class="ui_datepicker_tit" tabindex="0">' +
+                    title +
+                    "</p>"
+            );
+        }
+        $(".ui-datepicker-year").attr("title", "년도선택");
+        $(".ui-datepicker-month").attr("title", "월선택");
+    }, 1);
+}
+
+
 function calendar() {
     // 2022-06-28 달력 toggle인경우 추가
-    var $focusReturn2;
+    
     if ($(".datepicker").parents().hasClass("jsToggleDatepicker")) {
         $(".datepicker").datepicker({
             showOn: "button",
@@ -281,50 +290,8 @@ function calendar() {
                 "12",
             ],
             closeText: "닫기",
-            beforeShow: function (input, e) {
-                var self = $(input);
-                var title = self.attr("data-title");
-                setTimeout(function () {
-                    self.prop("readonly", true);
-                    setDatepicker(self);
-                    if (typeof title !== "undefined") {
-                        $(".ui-datepicker-header").prepend(
-                            '<p class="ui_datepicker_tit" tabindex="0">' +
-                                title +
-                                "</p>"
-                        );
-                    }
-                    $("#ui-datepicker-div")
-                        .attr({ tabindex: "0", "aria-hidden": "false" })
-                        .focus();
-                    $focusReturn2 = self.siblings(".ui-datepicker-trigger");
-                    $(".ui-datepicker-year").attr("title", "년도선택");
-                    $(".ui-datepicker-month").attr("title", "월선택");
-
-                    if (!$(".dim").length) {
-                        $(".wrap").append('<div class="dim"></div>');
-                    }
-                }, 1);
-
-                // 220502 레이어 높이가 달력보다 높은 case 때문에 생성
-                $(".layer_inner").css({ opacity: "0" });
-            },
-            onChangeMonthYear: function () {
-                var self = $(this);
-                var title = self.attr("data-title");
-                setTimeout(function () {
-                    setDatepicker(self);
-                    if (typeof title !== "undefined") {
-                        $(".ui-datepicker-header").append(
-                            '<p class="ui_datepicker_tit" tabindex="0">' +
-                                title +
-                                "</p>"
-                        );
-                    }
-                    $(".ui-datepicker-year").attr("title", "년도선택");
-                    $(".ui-datepicker-month").attr("title", "월선택");
-                }, 1);
-            },
+            beforeShow: beforeShow,
+            onChangeMonthYear: onChangeMonthYear,
             onClose: function (input, e) {
                 var self = $(this);
                 setTimeout(function () {
@@ -344,28 +311,7 @@ function calendar() {
         });
         //초기값을 오늘 날짜로 설정
         $(".datepicker").datepicker("setDate", "today"); //(-1D:하루전, -1M:한달전, -1Y:일년전), (+1D:하루후, -1M:한달후, -1Y:일년후)
-        function setDatepicker(target) {
-            var widgetHeader = target
-                .datepicker("widget")
-                .find(".ui-datepicker-header");
-            var prevYrBtn = $(
-                '<a class="ui-datepicker-prevY" data-handler="prevY"><span class="ui-icon">Prev Year</span></a>'
-            );
-            var nextYrBtn = $(
-                '<a class="ui-datepicker-nextY" data-handler="nextY"><span class="ui-icon">Next Year</span></a>'
-            );
-            nextYrBtn.appendTo(widgetHeader);
-            prevYrBtn.prependTo(widgetHeader);
-            prevYrBtn.unbind("click").bind("click", function () {
-                $.datepicker._adjustDate(target, -1, "Y");
-            });
-            nextYrBtn.unbind("click").bind("click", function () {
-                $.datepicker._adjustDate(target, +1, "Y");
-            });
-            if (target.hasClass("bg_dim")) {
-                $("body").append('<div class="dim"></div>');
-            }
-        }
+        
     }
     // [S] 2022-07-19 B1101 datepicker input 활성화 체크
     if ($(".dateItem .datepicker").length) {
